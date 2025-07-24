f9ca026cc25d6887b8de10e4558589a42299857d
Ilya Papich <Mark.Zaharenko@ya.ru>
2025-07-24 20:38:15 +0500

    fix

--- djangobnb_backend/property/models.py
+++ djangobnb_backend/property/models.py
@@ -18,6 +18,7 @@ class Property(models.Model):
     image = models.ImageField(upload_to='uploads/properties')
     landlord = models.ForeignKey(User, related_name='properties', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    favorited = models.ManyToManyField(User, related_name='favorites', blank=True)
     
     class Meta:
         verbose_name = ('Предложение')
